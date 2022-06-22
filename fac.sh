4a882eb5153b5e225efd9dd1a836bf16813faecd
Cloud User <ec2-user@ip-172-31-38-245.ap-south-1.compute.internal>
2022-06-22 01:00:19 +0000

    To find factorial of numbers

---
+++ fac.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+factorial()
+{
+n=$1
+facto=1
+for((i=1;i<=$n;i++))
+do
+	facto=`expr $facto \* $i`
+done
+echo "Factorial of $n is : $facto"
+}
+
+
+echo " Enter the numbers for which you want to find factorial : "
+read -a fact
+l=${#fact[@]}
+
+for((j=0;j<l;j++))
+do
+	factorial ${fact[$j]}
+done
+
+
